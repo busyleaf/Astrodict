a01e8e25ae7e37adc98ddcb24b959456adca0be2
busyleaf <rhsnipes@yahoo.com>
2017-04-17 20:13:24 -0700

    adding veiw controller

--- Astrology Dictionary/ViewController.swift
+++ Astrology Dictionary/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var astro = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagitarius", "Capricorn", "Aquarius", "Pisces"]
+    
+    @IBOutlet weak var astroDictTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        astroDictTableView.dataSource = self
+        astroDictTableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return astro.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        //give array index as tableview index
+        cell.textLabel?.text = astro[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
